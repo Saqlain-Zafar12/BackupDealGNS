7c35e3f40ce471d79ea6787a914027f82fe69e00
Saqlain-zafar12 <saqlainzafar342176@gmail.com>
2025-01-28 00:47:29 +0500

    ij

--- backend/server.js
+++ backend/server.js
@@ -45,12 +45,12 @@ app.use('/api/v1/dashboard', dashboardRoutes);
 app.use('/api/v1/delivery-types', deliveryTypeRoutes);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'tilt')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'tilt', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Error handling middleware
